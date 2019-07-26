ec2edc3bb02243c79c945aaa63252acd87cbb119
ducanh1296 <leducanh10296@gmail.com>
2019-07-26 19:14:24 +0700

    post

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\CreatePostRequest;
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+
+        return view('admin.post.index', ['posts' => $posts]);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.post.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreatePostRequest $request)
+    {
+        $data = $request->only([
+            'title',
+            'content',
+            'image',
+            'body',
+        ]);
+
+        $uploaded = $this->upload($data['image']);
+        
+        if (!$uploaded['status']) {
+            return back()->with('status', $uploaded['msg']);
+        }
+        $data['image'] = $uploaded['file_name'];
+        
+        try {
+            $post = Post::create($data);
+        } catch (Exception $e) {
+            return back()->with('status', __('message.create_fail'));
+        }
+        
+        return redirect()->route('admin.post.index')->with('status', __('post.status'));
+    }
+
+    private function upload($file) {
+        $destinationFolder = public_path() . '/' . config('post.image_path');
+        
+        try {
+            $fileName = date('Ymd_His') . '_' . $file->getClientOriginalName();
+            $imageFileType = $file->getClientOriginalExtension();
+            
+            if($imageFileType != 'jpg' && $imageFileType != 'png' && $imageFileType != 'jpeg' && $imageFileType != 'gif' ) {
+                $result = [
+                    'status' => false,
+                    'msg' => 'Sorry, only JPG, JPEG, PNG & GIF files are allowed.',
+                ];
+            }
+            // $mimeType = $file->getMimeType();
+            // $realPath = $file->getRealPath();
+            $file->move($destinationFolder, $fileName);
+                
+            $result = [
+                'status' => true,
+                'file_name' => $fileName,
+            ];
+        } catch (Exception $e) {
+            $msg = $e->getMessage();
+                
+            $result = [
+                'status' => false,
+                'msg' => $msg,
+            ];
+        }
+        return $result;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/admin/post/create.blade.php
@@ -0,0 +1,66 @@
+@extends('adminlte::page')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Create Posts') }}</div>
+                <div class="card-body">
+                    <form method="POST" action="{{ route('admin.post.store') }}" enctype="multipart/form-data">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="title" class="col-md-4 col-form-label text-md-right">{{ __('Title') }}</label>
+                            <div class="col-md-6">
+                                <input id="title" type="text" class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }}" name="title" required autofocus>
+                                @error('title')
+                                    <span>{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="image" class="col-md-4 col-form-label text-md-right">{{ __('image') }}</label>
+                            <div class="col-md-6">
+                                <input id="image" type="file" class="form-control{{ $errors->has('image') ? ' is-invalid' : '' }}" name="image" required>
+                                @error('image')
+                                    <span>{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="content" class="col-md-4 col-form-label text-md-right">{{ __('Content') }}</label>
+                            <div class="col-md-6">
+                                <input id="content" type="text" class="form-control{{ $errors->has('content') ? ' is-invalid' : '' }}" name="content" required autofocus>
+                                @error('content')
+                                    <span>{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <textarea name="body" id="editor">
+                            </textarea>
+                            <script>
+                                var simplemde = new SimpleMDE({ element: document.getElementById("editor") });
+                            </script>
+                        </div>
+                        
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Create') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
+<script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
